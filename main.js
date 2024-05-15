2145679c170d21b4c1c941d7547a184455ab1f70
alexismoreyra <alexismoreyra@hotmail.com>
2024-05-15 20:38:37 -0300

    Cambios_2

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-const fecha_cumpleaños = document.querySelector ('#fecha_cumpleaños')
-const boton = document.querySelector ('#boton_cumple')
-const resultado = document.querySelector ('#resultado')
+import moment from 'moment';
+const fecha_cumpleaños = document.querySelector('#fecha_cumpleaños')
+const boton = document.querySelector('#boton_cumple')
+const resultado = document.querySelector('#resultado')
 
-function calcularEdad (fecha) {
-  const fechaActual = moment ();
-  const fecha_cumpleaños = moment (fecha,'YYYY-MM-DD')
+function calcularEdad(fecha) {
+  const fechaActual = moment();
+  const fecha_cumpleaños = moment(fecha,'YYYY-MM-DD')
   
-  const edad = fechaActual.diff (fecha_cumpleaños,'years');
+  const edad = fechaActual.diff(fecha_cumpleaños,'years');
 
   return edad;
 }
@@ -14,6 +15,6 @@ function calcularEdad (fecha) {
 boton.addEventListener('click', ()=>{
   console.log(calcularEdad(fecha_cumpleaños.value));
 
-  resultado.innerText = calcularEdad (fecha_cumpleaños.value);
+  resultado.innerText = calcularEdad(fecha_cumpleaños.value);
 });
 
