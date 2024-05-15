af5a358420966f3800f571f5e4e1cd5d5cd0b28c
alexismoreyra <alexismoreyra@hotmail.com>
2024-05-15 20:29:36 -0300

    cambios_1

--- main.js
+++ main.js
@@ -2,24 +2,6 @@ const fecha_cumpleaños = document.querySelector ('#fecha_cumpleaños')
 const boton = document.querySelector ('#boton_cumple')
 const resultado = document.querySelector ('#resultado')
 
-boton.addEventListener('click', ()=>{
-    console.log('TOCARON EL BOTON')
-});
-
-function saludar () {
-    console.log ('SALUDANDO')
-};
-
-boton.addEventListener('click', ()=>{
-  console.log(fecha_cumpleaños.value)
-});
-
-boton.addEventListener('click', ()=>{
-  resultado.innerText = fecha_cumpleaños.value
-});
-
-//Funcion que en base a una fecha calcule la cantidad de años hasta el dia de hoy
-
 function calcularEdad (fecha) {
   const fechaActual = moment ();
   const fecha_cumpleaños = moment (fecha,'YYYY-MM-DD')
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+import { defineConfig } from 'vite';
+
+
+export default defineConfig({
+    base: '/app-calculadora-edad/',
+    build: {
+        outDir: 'docs',
+    },
+});
